bed08a22660d9d297603bbbf2f20724f9381b878
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-17 19:45:45 -0300

    first commit

---
+++ src/main/java/br/com/cobertura_testes/api/ProjetoCoberturaTestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cobertura_testes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCoberturaTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCoberturaTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cobertura_testes/api/ProjetoCoberturaTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cobertura_testes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCoberturaTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
