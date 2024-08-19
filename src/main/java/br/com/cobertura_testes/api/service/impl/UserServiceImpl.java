6b35e8f63aea10bcd181d4c8302ce8aa0f9e60a5
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-18 23:46:05 -0300

    Criando camada Repository.

--- src/main/java/br/com/cobertura_testes/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/cobertura_testes/api/service/impl/UserServiceImpl.java
@@ -1,15 +1,23 @@
 package br.com.cobertura_testes.api.service.impl;
 
 import br.com.cobertura_testes.api.domain.User;
+import br.com.cobertura_testes.api.repository.UserRepository;
 import br.com.cobertura_testes.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User findById(Long id) {
-        return null;
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
     }
 
 }
