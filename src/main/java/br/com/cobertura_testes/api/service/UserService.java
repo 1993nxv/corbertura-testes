51331d4bb1f735a786dbeb418500229bcbd01376
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-18 23:39:59 -0300

    Criando a camada service.

--- src/main/java/br/com/cobertura_testes/api/service/UserService.java
+++ src/main/java/br/com/cobertura_testes/api/service/UserService.java
@@ -1,9 +1,7 @@
 package br.com.cobertura_testes.api.service;
 
 import br.com.cobertura_testes.api.domain.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
 
     User findById(Long id);
--- src/main/java/br/com/cobertura_testes/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/cobertura_testes/api/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.cobertura_testes.api.service.impl;
 
 import br.com.cobertura_testes.api.domain.User;
 import br.com.cobertura_testes.api.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Override
