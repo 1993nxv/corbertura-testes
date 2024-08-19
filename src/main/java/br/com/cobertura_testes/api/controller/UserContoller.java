1935cb91d8d9ce31dda19b23008128e811c643cd
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-18 23:34:56 -0300

    Criando primeiro Endpoint findById para User.

--- src/main/java/br/com/cobertura_testes/api/controller/UserContoller.java
+++ src/main/java/br/com/cobertura_testes/api/controller/UserContoller.java
@@ -2,6 +2,7 @@ package br.com.cobertura_testes.api.controller;
 
 import br.com.cobertura_testes.api.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserContoller {
 
+    @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        return null;
+        return ResponseEntity.ok().body(new User(1L, "Wellington Delmondes", "del@mail.com", "123"));
     }
+
 }
